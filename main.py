2e8104aa366675a74675e1644da95106ebe2bb3a
Patryk Surmacz <patryksurmacz@gmail.com>
2023-06-13 12:10:10 +0200

    moved code to module

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import markdown_operations as md
 
 # Load environment variables from .env file
 load_dotenv()
@@ -7,40 +8,8 @@ load_dotenv()
 # Set the value of the OPENAI_API_KEY variable to the value of the environment variable "OPENAI_API_KEY"  # noqa: E501
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
-def get_file_names():
-    """
-    Returns a list of file names in the "alexandria" folder.
-    """
-    folder_path = "./alexandria"
-    file_names = os.listdir(folder_path)
-    file_names.remove('.obsidian')
-    return file_names
-
-def create_or_update_markdown_file(file_name, new_text):
-    """
-    Overwrites the contents of a markdown file with new text.
-
-    Args:
-        file_path (str): The path to the markdown file.
-        new_text (str): The new text to write to the file.
-    """
-    with open(f"./alexandria/{file_name}", "w", encoding="utf-8") as f:
-        f.write(new_text)
-
-def get_markdown_file_contents(file_name):
-    """
-    Returns the contents of a markdown file.
-
-    Args:
-        file_path (str): The path to the markdown file.
-    """
-    with open(f"./alexandria/{file_name}", "r", encoding="utf-8") as f:
-        text = f.read()
-    return text
-
-
 if __name__ == "__main__":
 
     # Get the list of file names in the "alexandria" folder
-    file_names = get_file_names()
+    file_names = md.get_file_names()
     print(f"File names: {file_names}")
